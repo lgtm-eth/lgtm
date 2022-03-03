43cb8ee2e08daa8aabe110379c36a3c68ab8f488
Daniel McCartney <dmccartney@gmail.com>
2022-03-03 01:23:52 -0300

    setup firebase web

--- web/src/index.js
+++ web/src/index.js
@@ -1,12 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import store from "./store/store";
+import { Box, CircularProgress } from "@mui/material";
+
+function PlaceholderApp() {
+  // TODO: cut this after placeholder release
+  return (
+    <Box sx={{ position: "relative", height: "100vh", width: "100vw" }}>
+      <Box
+        sx={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
+      >
+        <CircularProgress />
+      </Box>
+    </Box>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,7 +32,8 @@ ReactDOM.render(
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <BrowserRouter>
-          <App />
+          {/* TODO: <App />*/}
+          <PlaceholderApp />
         </BrowserRouter>
       </ThemeProvider>
     </Provider>
