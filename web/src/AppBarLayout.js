5926083c7d0d1ea3abdc7170c4d95bece0d89a33
Daniel McCartney <dmccartney@gmail.com>
2022-03-20 21:39:53 -0700

    app bar eye-following

--- web/src/AppBarLayout.js
+++ web/src/AppBarLayout.js
@@ -1,28 +1,11 @@
-import { AppBar, Box, Button, Input, Toolbar, Typography } from "@mui/material";
-import React from "react";
-import { Camera } from "@mui/icons-material";
-import WalletConnectProvider from "@walletconnect/web3-provider";
-import Web3Modal from "web3modal";
-import { ethers } from "ethers";
-
-function LogoLGTM(props) {
-  // TODO: proper logo image
-  return <Camera {...props} />;
-}
-
-function WordmarkLGTM(props) {
-  // TODO: proper wordmark image
-  return (
-    <Typography variant="h6" color="inherit" noWrap {...props}>
-      LGTM
-    </Typography>
-  );
-}
-
-function AppBarSearchInput() {
-  // TODO: stylized app bar search input
-  return <Input />;
-}
+import { AppBar, Box, Toolbar } from "@mui/material";
+import React, { useCallback, useState } from "react";
+// import WalletConnectProvider from "@walletconnect/web3-provider";
+// import Web3Modal from "web3modal";
+// import { ethers } from "ethers";
+import _ from "lodash";
+import Eyes from "./Eyes";
+import Wordmark from "./Wordmark";
 
 // const providerOptions = {
 //   walletconnect: {
@@ -41,78 +24,98 @@ function AppBarSearchInput() {
 //   providerOptions, // required
 // });
 
-async function attemptLogin() {
-  let _web3Modal = new Web3Modal({
-    network: "mainnet",
-    cacheProvider: true,
-    disableInjectedProvider: false,
-    providerOptions: {
-      walletconnect: {
-        package: WalletConnectProvider,
-        options: {
-          // TODO: infuraId: "INFURA_ID"
-        },
-      },
-    },
-  });
-  let instance = await _web3Modal.connect();
-  let injectedProvider = new ethers.providers.Web3Provider(instance);
-  let signer = injectedProvider.getSigner();
-  let signerAddress = await signer.getAddress();
-  let domain = {
-    name: "LGTM",
-    // TODO: more meta
-  };
-  let types = {
-    Visitor: [
-      { name: "wallet", type: "address" },
-      { name: "nonce", type: "string" },
-    ],
-    // TODO: more field types
-  };
-  let sig = await signer._signTypedData(domain, types, {
-    wallet: signerAddress,
-    nonce: "foobar",
-  });
-
-  // TODO: this server side
-  // let's see if it matches:
-  let verifiedAddress = ethers.utils.verifyTypedData(
-    domain,
-    types,
-    {
-      wallet: signerAddress,
-      nonce: "foobar",
-    },
-    sig
-  );
-  console.log({
-    sig,
-    verifiedAddress,
-    signerAddress,
-    match: verifiedAddress === signerAddress,
-  });
-}
-function login() {
-  return attemptLogin().catch((err) => console.log(err));
-}
+// async function attemptLogin() {
+//   let _web3Modal = new Web3Modal({
+//     network: "mainnet",
+//     cacheProvider: true,
+//     disableInjectedProvider: false,
+//     providerOptions: {
+//       walletconnect: {
+//         package: WalletConnectProvider,
+//         options: {
+//           // TODO: infuraId: "INFURA_ID"
+//         },
+//       },
+//     },
+//   });
+//   let instance = await _web3Modal.connect();
+//   let injectedProvider = new ethers.providers.Web3Provider(instance);
+//   let signer = injectedProvider.getSigner();
+//   let signerAddress = await signer.getAddress();
+//   let domain = {
+//     name: "LGTM",
+//     // TODO: more meta
+//   };
+//   let types = {
+//     Visitor: [
+//       { name: "wallet", type: "address" },
+//       { name: "nonce", type: "string" },
+//     ],
+//     // TODO: more field types
+//   };
+//   let sig = await signer._signTypedData(domain, types, {
+//     wallet: signerAddress,
+//     nonce: "foobar",
+//   });
+//
+//   // TODO: this server side
+//   // let's see if it matches:
+//   let verifiedAddress = ethers.utils.verifyTypedData(
+//     domain,
+//     types,
+//     {
+//       wallet: signerAddress,
+//       nonce: "foobar",
+//     },
+//     sig
+//   );
+//   console.log({
+//     sig,
+//     verifiedAddress,
+//     signerAddress,
+//     match: verifiedAddress === signerAddress,
+//   });
+// }
+// function login() {
+//   return attemptLogin().catch((err) => console.log(err));
+// }
 
 export default function AppBarLayout({ children }) {
+  let [eyes, setEyes] = useState({ clientX: 0, clientY: 0 });
+  // eslint-disable-next-line
+  let updateEyes = useCallback(
+    _.throttle(setEyes, 5, { leading: true, trailing: true }),
+    [setEyes]
+  );
   return (
-    <div style={{ paddingTop: 72 }}>
+    <div
+      style={{ paddingTop: 72 }}
+      onTouchMove={(e) =>
+        updateEyes({
+          clientX: e.touches[0].clientX,
+          clientY: e.touches[0].clientY,
+        })
+      }
+      onMouseMove={(e) =>
+        updateEyes({
+          clientX: e.clientX,
+          clientY: e.clientY,
+        })
+      }
+    >
       <AppBar enableColorOnDark>
         <Toolbar>
           <Box sx={{ display: "flex", alignItems: "center", flex: "auto" }}>
-            <LogoLGTM sx={{ mr: 2 }} />
-            <WordmarkLGTM sx={{ mr: 2 }} />
-            <AppBarSearchInput />
+            <Eyes {...eyes} size={0.4} sx={{ mr: 2 }} />
+            <Wordmark height={48} />
           </Box>
-          <Button color="inherit" variant="outlined" onClick={login}>
-            Login
-          </Button>
+          {/* TODO */}
+          {/*<Button color="inherit" variant="outlined" onClick={login}>*/}
+          {/*  Login*/}
+          {/*</Button>*/}
         </Toolbar>
       </AppBar>
-      <main>{children}</main>
+      <main style={{ minHeight: "100vh" }}>{children}</main>
     </div>
   );
 }
