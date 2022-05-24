f0cde0fe8fc84417f18b27af959d72ce4a4ed2e0
WJMcCartney <wjmccartney2002@gmail.com>
2022-05-24 17:39:00 -0400

    responded to comments

--- web/src/pages/About.js
+++ web/src/pages/About.js
@@ -1,10 +1,7 @@
 import AppBarLayout from "../components/AppBarLayout";
 import { Container, Box, Button, Checkbox } from "@mui/material";
-import {} from "@mui/icons-material/Star";
 import React from "react";
-
-const label = { inputProps: { "aria-label": "Checkbox demo" } };
-
+//this comment should not get removed
 export default function About() {
   return (
     <AppBarLayout>
@@ -13,7 +10,7 @@ export default function About() {
           <Button variant="contained" color="primary">
             Hello World
           </Button>
-          <Checkbox {...label} />
+          <Checkbox />
         </Box>
       </Container>
     </AppBarLayout>
