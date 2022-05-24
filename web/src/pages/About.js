4fadeb6b6ee348ca911a07e1847ee7e9c08d4be1
WJMcCartney <wjmccartney2002@gmail.com>
2022-05-24 16:49:48 -0400

    testing out committing stuff to the repo

--- web/src/pages/About.js
+++ web/src/pages/About.js
@@ -1,12 +1,20 @@
 import AppBarLayout from "../components/AppBarLayout";
-import { Container } from "@mui/material";
+import { Container, Box, Button, Checkbox } from "@mui/material";
+import {} from "@mui/icons-material/Star";
 import React from "react";
+
+const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
 export default function About() {
   return (
     <AppBarLayout>
       <Container sx={{ textAlign: "center", p: 4 }}>
-        {/*  TODO: play w/ mui here to feel out how we'll build the /about page */}
+        <Box sx={{ width: 200 }}>
+          <Button variant="contained" color="primary">
+            Hello World
+          </Button>
+          <Checkbox {...label} />
+        </Box>
       </Container>
     </AppBarLayout>
   );
