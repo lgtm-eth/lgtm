0f3f0194316dec00f744aab914e2b5c6eec99dac
WJMcCartney <wjmccartney2002@gmail.com>
2022-05-31 17:35:45 -0400

    putting up an about page for preview. needs responsivity

--- web/src/pages/About.js
+++ web/src/pages/About.js
@@ -1,17 +1,89 @@
 import AppBarLayout from "../components/AppBarLayout";
-import { Container, Box, Button, Checkbox } from "@mui/material";
+import { Container, Grid, Typography } from "@mui/material";
 import React from "react";
-//this comment should not get removed
+import Wordmark from "../Wordmark";
+
 export default function About() {
   return (
     <AppBarLayout>
-      <Container sx={{ textAlign: "center", p: 4 }}>
-        <Box sx={{ width: 200 }}>
-          <Button variant="contained" color="primary">
-            Hello World
-          </Button>
-          <Checkbox />
-        </Box>
+      <Container sx={{ textAlign: "center", p: 4, mt: 10 }}>
+        <Grid container spacing={0} columns={{ xs: 4, sm: 4, md: 24 }}>
+          <Grid item sm={-1000} md={4} sx={{ m: 0, p: 0 }}>
+            <svg height="200" width="200">
+              <circle
+                cx="100"
+                cy="100"
+                r="80"
+                stroke="black"
+                stroke-width="3"
+                fill="blue"
+              />
+            </svg>
+          </Grid>
+          <Grid item xs={4} md={16}>
+            <Wordmark height={300} />
+            <Typography variant="h4" sx={{ pb: 5 }}>
+              Building Tools for Trust
+            </Typography>
+            <Typography variant="body1" textAlign="left">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.{" "}
+              <br />
+              <br />
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.{" "}
+              <br />
+              <br />
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.{" "}
+              <br />
+              <br />
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.{" "}
+              <br />
+              <br />
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </Typography>
+          </Grid>
+          <Grid item sm={1000} md={4} sx={{ mt: 50 }}>
+            <svg height="200" width="200">
+              <circle
+                cx="100"
+                cy="100"
+                r="80"
+                stroke="black"
+                stroke-width="3"
+                fill="green"
+              />
+            </svg>
+          </Grid>
+        </Grid>
       </Container>
     </AppBarLayout>
   );
